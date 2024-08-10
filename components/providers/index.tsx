95008654e372e8eaafd89a2257328d95aa6bd07e
sa96125 <sa96125@gmail.com>
2024-08-10 23:07:37 +0900

    feat: Configure theme support for Next.js with dark mode

---
+++ components/providers/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { ThemeProvider } from "../../app/theme-provider";
+
+function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
+export default Providers;
