42a2ad8bcac3f526d140ec27d97873c6eeb8a5dc
sa96125 <sa96125@gmail.com>
2024-08-16 11:58:18 +0900

    feat: add MacMenuBar, MacFolder, MacBrowser, and Dock components to HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,42 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
+import Dock from "@/components/mac/Dock";
+import MacMenuBar from "@/components/mac/MacMenuBar";
+import MacBrowser from "@/components/mac/Safari";
+import MacFolder from "@/components/mac/MacForder";
 
 function HomePage() {
   return (
     <div className="relative h-screen w-full">
+      {/* 상단 공통 메뉴바 */}
+      <MacMenuBar />
+
+      {/* 월페이퍼 */}
       <Image
         src="/bigsur_wallpaper.jpg"
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
-        alt=""
-        className="object-cover z-10"
+        alt="tars os wallpaper"
+        className="object-cover -z-10"
       />
-      <h1 className="text-white text-4xl">HomePage</h1>
+
+      {/* 컨텐츠 */}
+      <div
+        className="relative w-full pt-[33px]"
+        style={{ height: "calc(100vh - 95px)" }}
+      >
+        <MacFolder />
+
+        <MacBrowser
+          src="/경력기술서.pdf"
+          title="경력기술서"
+          width="80%"
+          height="90%"
+        />
+      </div>
+
+      {/* 하단 공통 Dock */}
+      <Dock />
     </div>
   );
 }
